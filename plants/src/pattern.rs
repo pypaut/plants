586076a1066eab7747943c54a3e6d24eacf73387
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-03-06 14:26:34 +0100

    Plants : add randomization parameter.

--- plants/src/pattern.rs
+++ plants/src/pattern.rs
@@ -1,14 +1,24 @@
+use rand::{thread_rng, Rng};
+
+
 pub struct Pattern {
-    pub pattern : char,
-    pub replacement : String
+    pub pattern : char,       // Initial character
+    pub replacement : String, // Replacement string
+    pub p : f32               // Replacement probability
 }
 
 impl Pattern {
-    pub fn new(p : char, r : String) -> Pattern {
-        Pattern{pattern: p, replacement: r}
+    pub fn new(pat : char, r : String, p : f32) -> Pattern {
+        Pattern{pattern: pat, replacement: r, p: p}
     }
 
-    pub fn test(&self, c : char) -> bool {
-        c == self.pattern
+    pub fn test(&self, c : char, p : f32) -> bool {
+        let mut rng = thread_rng();
+        if rng.gen_bool(p.into()) {
+            c == self.pattern
+        }
+        else {
+            false
+        }
     }
 }
