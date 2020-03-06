324dd1f7dde7cabea16bc452c21ce6256a224cce
Clement Rebut <clement.rebut@epita.fr>
2020-03-06 17:01:29 +0100

    Proto: Finished turtle

--- graphics/graph3d/src/main.rs
+++ graphics/graph3d/src/main.rs
@@ -1,5 +1,22 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul};
+use std::f64::consts::PI;
 
+struct Mat3 {
+    data : Vec<Vec<f64>>
+}
+
+impl Mat3 {
+    fn new(data : Vec<Vec<f64>>) -> Mat3 {
+        Mat3{data}
+    }
+
+    fn get(&self, i : usize, j : usize) -> f64 {
+        self.data[i][j]
+    }
+}
+
+#[derive(Debug)]
+#[derive(Clone, Copy)]
 struct Vector3 {
     x: f64,
     y: f64,
@@ -9,6 +26,58 @@ struct Vector3 {
 impl Vector3 {
     fn new(x: f64, y: f64, z: f64) -> Vector3 {
         Vector3{x, y, z}
+    }
+
+    fn cross(&self, other : Vector3) -> Vector3 {
+        Vector3{x: self.y * other.z - self.z * other.y,
+        y: self.z * other.x - self.x * other.z,
+        z: self.x * other.y - self.y * other.x}
+    }
+
+    fn get(&self, i : usize) -> f64 {
+        if i == 0 {
+            self.x
+        }
+        else if i == 1 {
+            self.y
+        }
+        else {
+            self.z
+        }
+    }
+
+    fn set(&mut self, value : f64, i : usize) {
+        if i == 0 {
+            self.x = value;
+        }
+        else if i == 1 {
+            self.y = value;
+        }
+        else {
+            self.z = value;
+        }
+    }
+
+    fn apply(&self, mat : &Mat3) -> Vector3 {
+        let mut res = Vector3::new(0f64, 0f64, 0f64);
+
+        for i in 0..3 {
+            let mut tmp = 0f64;
+            for j in 0..3 {
+                tmp += self.get(j) * mat.get(i, j);
+            }
+            res.set(tmp, i);
+        }
+
+        res
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vector3::new(self.x * rhs, self.y * rhs, self.z * rhs)
     }
 }
 
@@ -32,6 +101,7 @@ impl Sub for Vector3 {
     }
 }
 
+#[derive(Debug)]
 struct Turtle {
     pos: Vector3,
     heading: Vector3,
@@ -44,24 +114,45 @@ impl Turtle {
     fn new() -> Turtle {
         Turtle{pos: Vector3::new(0f64, 0f64, 0f64),
         heading: Vector3::new(0f64, 0f64, 1f64),
-        left: Vector3::new(-1f64, 0f64, -0f64),
-        up: Vector3::new(0f64, -1f64, 0f64)}
+        left: Vector3::new(0f64, -1f64, 0f64),
+        up: Vector3::new(1f64, 0f64, 0f64)}
     }
 
-    fn forward(mut self, dist: f64) {
-        self.pos = self.pos + self.heading;
+    fn forward(&mut self, dist: f64) {
+        self.pos = self.pos + self.heading * dist;
     }
     
-    fn rot_pitch(mut self, angle: f64) {
+    fn rot_pitch(&mut self, a: f64) {
+        let mat = Mat3{data: vec![
+        vec![a.cos(), 0.0, -a.sin()],
+        vec![0.0, 1.0, 0.0],
+        vec![a.sin(), 0.0, a.cos()]]};
 
+        self.heading = self.heading.apply(&mat);
+        self.left = self.left.apply(&mat);
+        self.up = self.up.apply(&mat);
     }
 
-    fn rot_roll(mut self, angle: f64) {
+    fn rot_roll(&mut self, angle: f64) {
+        let mat = Mat3{data: vec![
+            vec![1.0, 0.0, 0.0],
+            vec![0.0, a.cos(), -a.sin()],
+            vec![0.0, a.sin(), a.cos()]]};
 
+        self.heading = self.heading.apply(&mat);
+        self.left = self.left.apply(&mat);
+        self.up = self.up.apply(&mat);
     }
 
-    fn rot_yaw(mut self, angle: f64) {
+    fn rot_yaw(&mut self, angle: f64) {
+        let mat = Mat3{data: vec![
+            vec![a.cos(), a.sin(), 0.0],
+            vec![-a.sin(), a.cos(), 0.0],
+            vec![0.0, 0.0, 1.0]]};
 
+        self.heading = self.heading.apply(&mat);
+        self.left = self.left.apply(&mat);
+        self.up = self.up.apply(&mat);
     }
 }
 
@@ -78,4 +169,8 @@ fn read_str(s: &str) {
 
 fn main() {
     println!("Hello, world!");
+    let mut t = Turtle::new();
+    println!("{:?}", &t);
+    t.rot_pitch(PI/2.0);
+    println!("{:?}", &t);
 }
